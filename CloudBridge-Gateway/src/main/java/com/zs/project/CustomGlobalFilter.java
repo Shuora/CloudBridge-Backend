69ad149c7980c3616a64e14bd70e27a6945e6be6
庄朔 <1109896198@qq.com>
2024-07-18 11:07:07 +0800

    feat(sentinel): 添加了服务限流和服务熔断

--- CloudBridge-Gateway/src/main/java/com/zs/project/CustomGlobalFilter.java
+++ CloudBridge-Gateway/src/main/java/com/zs/project/CustomGlobalFilter.java
@@ -3,10 +3,10 @@ package com.zs.project;
 
 import com.zs.project.domain.entity.InterfaceInfo;
 import com.zs.project.domain.entity.User;
-import com.zs.project.utils.SignUtils;
 import com.zs.project.service.InnerInterfaceInfoService;
 import com.zs.project.service.InnerUserInterfaceInfoService;
 import com.zs.project.service.InnerUserService;
+import com.zs.project.utils.SignUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.reactivestreams.Publisher;
@@ -51,9 +51,16 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
     @DubboReference
     private InnerUserInterfaceInfoService innerUserInterfaceInfoService;
 
+    /**
+     * 网关过滤器，用于请求的鉴权和路由转发。
+     *
+     * @param exchange 服务器web交换机，提供请求和响应的交互信息。
+     * @param chain    网关过滤器链，用于继续处理过滤链中的下一个过滤器。
+     * @return Mono<Void>，表示异步处理结果。
+     */
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-
+        // 记录请求相关信息
         // 1. 日志
         ServerHttpRequest request = exchange.getRequest();
         log.info("请求唯一标识" + request.getId());
@@ -67,11 +74,15 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
         log.info("请求头" + request.getHeaders());
         log.info("请求体" + request.getBody());
         ServerHttpResponse response = exchange.getResponse();
+
+        // 对请求来源进行访问控制，如果不是白名单内的IP，则禁止访问
         // 2. 访问控制 -（黑白名单）
         if (!IP_WHITE_LIST.contains(hostName)) {
             response.setStatusCode(HttpStatus.FORBIDDEN);
             return response.setComplete();
         }
+
+        // 进行用户鉴权，检查请求头中的accessKey是否有效
         // 3. 用户鉴权（判断 AK、SK 是否合法）
         HttpHeaders headers = request.getHeaders();
         String accessKey = headers.getFirst("accessKey");
@@ -88,23 +99,22 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
         if (invokeUser == null) {
             return handlerNoAuth(response);
         }
-//        if (!accessKey.equals("zs")) {
-//            return handlerNoAuth(response);
-//        }
-        // 防止放高并发
+        // 防止高并发攻击
         if (Long.parseLong(nonce) > 1000000L) {
             return handlerNoAuth(response);
         }
-        // 时间和当前时间不能超过 5 分钟
+        // 检查请求时间是否过期
         if (System.currentTimeMillis() - Long.parseLong(timestamp) > 1000 * 60 * 5) {
             return handlerNoAuth(response);
         }
+        // 验证签名是否匹配
         String secretKey = invokeUser.getSecretKey();
         String serverSign = SignUtils.getSign(body, secretKey);
-//        String serverSign = SignUtils.getSign(body, "123456");
         if (sign == null || !sign.equals(serverSign)) {
             return handlerNoAuth(response);
         }
+
+        // 检查请求的接口是否存在以及请求方法是否匹配
         // 4. 请求的模拟接口是否存在？以及请求方法是否匹配？
         InterfaceInfo interfaceInfo = null;
         try {
@@ -115,6 +125,8 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
         if (interfaceInfo == null) {
             return handlerNoAuth(response);
         }
+
+        // 路由转发，调用相应的模拟接口
         // 5. 请求转发，调用模拟接口
 //        Mono<Void> filter = chain.filter(exchange);
 //        log.info("响应", response.getStatusCode());
@@ -123,42 +135,58 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
         return handleResponse(exchange, chain, interfaceInfo.getId(), invokeUser.getId());
     }
 
+
     /**
      * 处理响应
      *
-     * @param exchange
-     * @param chain
-     * @return
+     * @param exchange 服务器web交换信息，包含请求和响应信息
+     * @param chain 网关过滤器链，用于继续处理过滤器链中的下一个过滤器
+     * @param interfaceInfoId 接口信息ID，用于标识调用的接口
+     * @param userId 用户ID，用于标识调用接口的用户
+     * @return Mono<Void> 表示异步处理完成后不返回任何结果
      */
     public Mono<Void> handleResponse(ServerWebExchange exchange, GatewayFilterChain chain, long interfaceInfoId, long userId) {
         try {
+            // 获取原始响应对象
             ServerHttpResponse originalResponse = exchange.getResponse();
+            // 获取数据缓冲区工厂，用于创建和管理数据缓冲区
             // 缓存数据的工厂
             DataBufferFactory bufferFactory = originalResponse.bufferFactory();
+            // 获取响应状态码
             // 拿到响应码
             HttpStatus statusCode = (HttpStatus) originalResponse.getStatusCode();
+
+            // 当响应状态码为HTTP 200 OK时，对响应进行进一步处理
             if (statusCode == HttpStatus.OK) {
+                // 创建装饰过的响应对象，用于在写入响应体时进行额外的操作
                 // 装饰，增强能力
                 ServerHttpResponseDecorator decoratedResponse = new ServerHttpResponseDecorator(originalResponse) {
+                    // 覆写写入响应体的方法
                     // 等调用完转发的接口后才会执行
                     @Override
                     public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
+                        // 判断响应体是否为Flux类型
                         log.info("body instanceof Flux: {}", (body instanceof Flux));
                         if (body instanceof Flux) {
+                            // 将Publisher转换为Flux，以便进行操作
                             Flux<? extends DataBuffer> fluxBody = Flux.from(body);
+                            // 对每个数据缓冲区进行处理
                             // 往返回值里写数据
                             // 拼接字符串
                             return super.writeWith(
                                     fluxBody.map(dataBuffer -> {
+                                        // 更新接口调用次数
                                         // 7. 调用成功，接口调用次数 + 1 invokeCount
                                         try {
                                             innerUserInterfaceInfoService.invokeCount(interfaceInfoId, userId);
                                         } catch (Exception e) {
                                             log.error("invokeCount error", e);
                                         }
+                                        // 读取并释放数据缓冲区的内容
                                         byte[] content = new byte[dataBuffer.readableByteCount()];
                                         dataBuffer.read(content);
                                         DataBufferUtils.release(dataBuffer);//释放掉内存
+                                        // 构造新的数据缓冲区
                                         // 构建日志
                                         StringBuilder sb2 = new StringBuilder(200);
                                         List<Object> rspArgs = new ArrayList<>();
@@ -170,21 +198,27 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
                                         return bufferFactory.wrap(content);
                                     }));
                         } else {
+                            // 对于非Flux类型的响应体，记录错误日志
                             // 8. 调用失败，返回一个规范的错误码
                             log.error("<--- {} 响应code异常", getStatusCode());
                         }
                         return super.writeWith(body);
                     }
                 };
+                // 使用装饰过的响应对象继续处理过滤器链
                 // 设置 response 对象为装饰过的
                 return chain.filter(exchange.mutate().response(decoratedResponse).build());
             }
+            // 如果响应状态码不是HTTP 200 OK，则直接继续处理过滤器链
             return chain.filter(exchange); // 降级处理返回数据
         } catch (Exception e) {
+            // 捕获并记录异常
             log.error("网关处理响应异常" + e);
+            // 继续处理过滤器链
             return chain.filter(exchange);
         }
     }
+
 
 
     @Override
--- CloudBridge-Service/src/main/java/com/zs/project/config/MyBatisPlusConfig.java
+++ CloudBridge-Service/src/main/java/com/zs/project/config/MyBatisPlusConfig.java
@@ -1,4 +1,4 @@
-package project.config;
+package com.zs.project.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
